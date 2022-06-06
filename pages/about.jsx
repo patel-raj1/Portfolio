e39e3c4a03647ac09b92d6650d5d4fd1347d5d73
Kaushal Patel <mr.kaushal.p@gmail.com>
2022-06-06 16:36:48 +0530

    About Changed

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,8 @@ const AboutPage = () => {
   return (
     <>
       <h3>A Little Bit About Me</h3>
+      <br />
+      <p>Web designer from Gujarat, India. I have rich experience in website design and building, also I am good at Django and WordPress. I love to talk with you about the idea of our unique project.</p>
     </>
   );
 };
