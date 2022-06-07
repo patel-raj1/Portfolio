3fb71a8808c7a5dcb2359b340d1249196a2cdda6
Kaushal Patel <mr.kaushal.p@gmail.com>
2022-06-07 15:11:13 +0530

    Index Page Changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,8 @@ export default function HomePage() {
       <div className={styles.container}>
         <div className={styles.background}>
           <h1>I BUILD</h1>
-          <h1>WEBSITES</h1>
+          <h1>COOL</h1>
+          <h1>STUFF</h1>
         </div>
         <div className={styles.foreground}>
           <div className={styles.content}>
