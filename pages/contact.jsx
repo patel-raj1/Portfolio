084c2cf37fa3e74c5063e0156094278dccede2bb
Kaushal Patel <mr.kaushal.p@gmail.com>
2022-06-08 11:01:01 +0530

    Working on Form Submission

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -11,7 +11,7 @@ const ContactPage = () => {
   const submitForm = async (e) => {
     e.preventDefault();
     console.log(process.env.NEXT_PUBLIC_API_URL);
-    const res = await fetch('https://kaushalpatel.info/api/contact', {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
       method: 'POST',
       body: JSON.stringify({ name, email, subject, message }),
     });
