160f00e7e85244b04177e38001da07c425e2ff71
Kaushal Patel <55987371+mrkaushal@users.noreply.github.com>
2022-10-03 18:32:23 +0000

    About Page Updated

--- pages/about.jsx
+++ pages/about.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
       <h3>A Little Bit About Me</h3>
       <br />
       <p>
-        Web designer from Gujarat, India. I have rich experience in website design and building, also I am good at Django and WordPress. 
+        Web designer from Gujarat, India. I have rich experience in website design and building, also I am good at Django and WordPress.
         I love to talk with you about the idea of our unique project.
       </p>
       <br />
@@ -17,10 +17,10 @@ const AboutPage = () => {
         <div className={styles.card}>
           <div>
             <img className={styles.left_img} src="/edu/charusat.jpg" alt="charusat" />
-            <h3 className={styles.title}>Charotar University of Science & Technology</h3>
-            <span className={styles.span}>B. Tech. C.S.E. (2021 - 2024)</span>
+            <h3 className={styles.title}>CHARUSAT University, Anand</h3>
+            <span className={styles.span}>B.Tech - Computer Science (2021 - 2024)</span>
           </div>
-          <p className={styles.desc}><b>Grade: CGPA - 8.11</b><br/>I have attended the Blockchain Technology workshop, also participated in 30 Days of Google Cloud Challenge. I am also participating in hackathons and other events.</p>
+          <p className={styles.desc}><b>Grade: CGPA - 8.11</b><br/>I have attended the Blockchain Technology workshop, also participated in 30 Days of Google Cloud Challenge. I am also participating in hackathons and other events.<br /></p>
         </div>
         <div className={styles.card}>
           <div>
@@ -28,7 +28,7 @@ const AboutPage = () => {
             <h3 className={styles.title}>Amity University Online</h3>
             <span className={styles.span}>B.B.A. - General (2021 - 2024)</span>
           </div>
-          <p className={styles.desc}><b>Grade: SGPA - 8.00</b><br/>I have completed Advance Excel course from Amity University as a part of my B.B.A Career. I have also attended workshops that was being held by working professional<br /><br /></p>
+          <p className={styles.desc}><b>Grade: SGPA - 8.00</b><br />I have completed Advance Excel course from Amity University as a part of my B.B.A Career. I have also attended workshops that was being held by working professional<br /></p>
         </div>
       </div>
       <div className={styles.container}>
@@ -38,7 +38,7 @@ const AboutPage = () => {
             <h3 className={styles.title}>Gujarat Technological University</h3>
             <span className={styles.span}>Diploma C.E. (2018 - 2021)</span>
           </div>
-          <p className={styles.desc}><b>Grade: CGPA - 9.50</b><br/>I have completed Diploma in computer engineering from OM Institute of Engineering & Technology, Junagadh, Gujarat. I have completed many projects and one of them is Hospital Management System using Django framework that works on Python.</p>
+          <p className={styles.desc}><b>Grade: CGPA - 9.50</b><br />I have completed Diploma in computer engineering from OM Institute of Engineering & Technology, Junagadh, Gujarat. I have completed many projects and one of them is Hospital Management System using Django framework that works on Python.</p>
         </div>
         <br />
       </div>
